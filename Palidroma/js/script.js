a1ee6e246ba5f38eaad335ece77a291fd8ef3cda
mikzanco <mikyzancolo97@gmail.com>
2022-10-19 17:29:25 +0200

    [add] split per dividere l parola inserita con il prompt in un array di lettere

--- Palidroma/js/script.js
+++ Palidroma/js/script.js
@@ -1,2 +1,18 @@
 // Chiedere all’utente di inserire una parola
 //Creare una funzione per capire se la parola inserita è palindroma
+
+let parola = prompt('scrivi una parola e ti dirò se è palindroma');
+
+let parolaDivisa = parola.split('')
+
+console.log(parolaDivisa);
+// let parolaInversa = invertiParola(parola);
+
+// if(parola == parolaInversa){
+//     console.log('la parola è palindroma');
+// }else{
+//     console.log('no è palindroma');
+// }
+
+
+// console.log(parola);
