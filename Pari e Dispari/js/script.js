938ec76c285b79f1964950eec9be622542708c72
mikzanco <mikyzancolo97@gmail.com>
2022-10-19 23:57:52 +0200

    add function pariDispari

--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -3,3 +3,29 @@
 // Sommiamo i due numeri
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
+
+const pariDispari = prompt('scegli e scrivi o pari o dispari.');
+const pickNumberU = prompt ('scegli un numero da 1 a 5');
+console.log(pariDispari);
+console.log(pickNumberU);
+const numeriPc = [1, 2, 3, 4, 5];
+
+const randomIndexPc = Math.floor(Math.random() * numeriPc.length);
+    console.log(randomIndexPc);
+
+    let
+
+function pcPickNumber(){
+    
+    
+    if (([pickNumberU + randomIndexPc] % 2 === 0) && (pariDispari === 'pari')) {
+        
+        return 'hai vinto';
+    
+    }else if (!([pickNumberU + randomIndexPc] % 2 === 0) && (pariDispari === 'dispari')) {
+        
+        return 'hai vinto';
+    }else{
+        return 'ha vinto il pc';
+    }
+}
