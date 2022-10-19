7e917aee26c5c13d63f14ec31fb7ecc5e1a96ce5
mikzanco <mikyzancolo97@gmail.com>
2022-10-19 17:59:20 +0200

    add function per trovare le parole palindrome

--- Palidroma/js/script.js
+++ Palidroma/js/script.js
@@ -1,18 +1,41 @@
 // Chiedere all’utente di inserire una parola
 //Creare una funzione per capire se la parola inserita è palindroma
 
+
+
+
+
+// trasformo la parola in un array di lettere che compongono la parola.
+
+
+// parolaDivisa che ora è un array lo giro con il metodo revese
+
+
+// console.log(parolaDivisa_g);
+// unisco di nuovo le lettere che componog l'array per comporre la parola girata.
+
+
+// console.log(parolaInvertita);
+// adesso confornto le due parole.
+
+
+
+
 let parola = prompt('scrivi una parola e ti dirò se è palindroma');
 
-let parolaDivisa = parola.split('')
+let parolaInversa = invertiParola(parola);
 
-console.log(parolaDivisa);
-// let parolaInversa = invertiParola(parola);
+if(parola == parolaInversa){
+    console.log('la parola è palindroma');
+  } else {
+    console.log('la parola non è palindroma');
+  }
+  
+function invertiParola(str){
+  var strInversa = str.split('').reverse().join('');  
+  return strInversa;
+}
 
-// if(parola == parolaInversa){
-//     console.log('la parola è palindroma');
-// }else{
-//     console.log('no è palindroma');
-// }
 
 
 // console.log(parola);
